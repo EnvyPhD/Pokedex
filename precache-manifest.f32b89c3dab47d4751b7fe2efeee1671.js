520d035a877c93b3048b958b92cef95499e85355
Marius J. H. Johansen <twiller3@hotmail.com>
2020-06-10 16:56:28 +0200

    Updates

---
+++ precache-manifest.f32b89c3dab47d4751b7fe2efeee1671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c135e79dcc386940dd0df67a9e660be1",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "dd035e890a001b51a61a",
+    "url": "/pokedex/static/css/main.da566504.chunk.css"
+  },
+  {
+    "revision": "d21e7b4cf47a6a6ed12c",
+    "url": "/pokedex/static/js/2.39be9363.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.39be9363.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd035e890a001b51a61a",
+    "url": "/pokedex/static/js/main.bca9719e.chunk.js"
+  },
+  {
+    "revision": "d6a68d44d70a718fe8be",
+    "url": "/pokedex/static/js/runtime-main.1c862034.js"
+  }
+]);
