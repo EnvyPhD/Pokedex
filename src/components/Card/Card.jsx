5fff131ffe5e01896e10a958a311bcabbda89f5d
Marius J. H. Johansen <twiller3@hotmail.com>
2020-06-11 02:00:32 +0200

    fixed props. Helperfunction for cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
-
-
 import './Card.css'
+
+function capitalizeFirstLetter (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
 export const Card = props => (
+    // console.log(props),
     <div className="Cards">
-            {/* {console.log(props)
-            } */}
+
         <img alt="friend" src ={`https://robohash.org/${props.pokemon.id}?set=set2&size=180x180`}></img>
-        <h2> {props.pokemon.name} </h2>
-        <p> {props.pokemon.types}</p>
+        <h2> 
+            {capitalizeFirstLetter(props.pokemon.name)} 
+        </h2>
+        <p>Type: {props.pokemon.types[0].type.name} </p>
+
     </div>
-)
+)   
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Card } from '../Card/Card'
 import './CardList.css'
 
+
+
 export const CardList = props => (
-    
+    console.log(props),
     <div className='CardList'>
-        {props.friends.map(pokemon => (
-            <Card key={pokemon.id} pokemon={pokemon} />
+        {props.names.map(pokemon => (
+            <Card key={pokemon.id} pokemon={pokemon}  />
         ))}
     </div>
 )
